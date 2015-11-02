8b8950721acaac00dffd684f8061ac726dffd2ef
R JAG <jagan.kumar.rajamani@verizon.com>
2015-11-02 02:51:55 -0800

    parser code checked in

--- App/src/Parser.java
+++ App/src/Parser.java
@@ -33,19 +33,22 @@ public class Parser {
 	}
 	
 	
-	public static void dbCall(List<String> list) throws ClassNotFoundException, SQLException{
-		  if(list.size() > 0)
+	public static void dbCall(List<String> list) {
+		try{  
+			if(list.size() == 0)
 			list.add(0,"30");list.add(1,"0");list.add(2,"100.00%");	list.add(3,"124 ms");list.add(4,"62 ms");list.add(5,"353 ms");
-		  Class.forName("com.mysql.jdbc.Driver");
-	      System.out.println("Connecting to database...");
-	      conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/myapp_test","travis","");
-	      System.out.println("Creating statement.......");
-	      stmt = conn.createStatement();
-	      String sql;
-	      sql = "insert into report values('"+list.get(0)+"', '"+list.get(1)+"','"+list.get(2)+"','"+list.get(3)+"','"+list.get(4)+"','"+list.get(5)+"')";
-	      System.out.println("SQL QUERY = " + sql);
-	      stmt.executeUpdate(sql);
-	      System.out.println("QUERY EXECUTED SUCCESSFULLY ...");
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/myapp_test","travis","");
+			System.out.println("Creating statement.......");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "insert into report values('"+list.get(0)+"', '"+list.get(1)+"','"+list.get(2)+"','"+list.get(3)+"','"+list.get(4)+"','"+list.get(5)+"')";
+			System.out.println("SQL QUERY = " + sql);
+			stmt.executeUpdate(sql);
+			System.out.println("QUERY EXECUTED SUCCESSFULLY ...");
+		}catch(Exception ex){
+		}
 	}
 	
 }
